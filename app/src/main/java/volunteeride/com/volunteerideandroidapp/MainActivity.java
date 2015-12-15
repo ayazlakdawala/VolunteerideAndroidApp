ca6f6eb35754069a8a81b5c069c5724eb289bc62
Ayaz Lakdawala <ayazlakdawala@gmail.com>
2015-12-15 18:16:47 -0500

    Added changes to make authorized rest api calls to the server and store the session id in SharedPreferences

--- app/src/main/java/volunteeride/com/volunteerideandroidapp/MainActivity.java
+++ app/src/main/java/volunteeride/com/volunteerideandroidapp/MainActivity.java
@@ -1,5 +1,7 @@
 package volunteeride.com.volunteerideandroidapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -31,14 +33,26 @@ public class MainActivity extends AppCompatActivity {
     private Button bttnRetrieveCenters;
     private ListView lstViewRides;
     List<Ride> rides;
+    public static final String VOLUNTEERIDE_PREFERENCES = "VolRidePrefs" ;
+
+    SharedPreferences sharedpreferences;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Add data in shared preferences.
+        sharedpreferences = getSharedPreferences(VOLUNTEERIDE_PREFERENCES,
+                Context.MODE_PRIVATE);
+
+//        SharedPreferences.Editor editor = sharedpreferences.edit();
+//        editor.putString("JSESSIONID", "BA1B0B180C0AAA3478F052B095B0B731");
+//        editor.commit();
 
         bttnRetrieveCenters = (Button)findViewById(R.id.bttnRtrvRides);
         lstViewRides = (ListView)findViewById(R.id.lstViewRides);
@@ -86,8 +100,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected List<Ride> doInBackground(String... params) {
 
-            HttpAuthentication authHeader = new HttpBasicAuthentication("rideusr", "password");
             HttpHeaders requestHeaders = new HttpHeaders();
+
+            //add session id to request
+            String jsessionId = sharedpreferences.getString("JSESSIONID", null);
+            //requestHeaders.add("Cookie", "JSESSIONID=" + jsessionId);
+
+            //add basic authentication header
+            HttpAuthentication authHeader = new HttpBasicAuthentication("rideusr", "password");
             requestHeaders.setAuthorization(authHeader);
             HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
 
@@ -96,17 +116,26 @@ public class MainActivity extends AppCompatActivity {
 
             String URL = "http://10.0.2.2:8080/volunteeride/centers/564798040364519acbc255b1/rides";
 
-           // ResponseEntity<Center[]> result =  restTemplate.getForEntity("http://10.0.2.2:8080/volunteeride/centers", Center[].class);
-
-
             ResponseEntity<PagedResponse> response = null;
             try {
                 response = restTemplate.exchange(URL, HttpMethod.GET, requestEntity, PagedResponse.class);
             } catch (Exception e) {
+                //TODO Handle UnAuthorized errors
                 e.printStackTrace();
             }
 
             PagedResponse result = response.getBody();
+
+            //retrieve session id from response
+            String SetCookieHeaderValue = response.getHeaders().get("Set-Cookie").get(0);
+            int index = SetCookieHeaderValue.indexOf("JSESSIONID=");
+            int endIndex = SetCookieHeaderValue.indexOf(";", index);
+            String sessionID = SetCookieHeaderValue.substring(
+                    index + "JSESSIONID=".length(), endIndex);
+
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putString("JSESSIONID", sessionID);
+            editor.commit();
 
             rides = Arrays.asList((Ride[])result.getContent());
 
